4fdea1fe1c2c37efb87472e5d573bdaffe73f98d
Nikolaos Katzakis <2638631+joobei@users.noreply.github.com>
2021-11-26 00:23:25 +0200

    discarded form generator stuff

--- src/components/Single_Match.vue
+++ src/components/Single_Match.vue
@@ -1,22 +1,13 @@
 <template>
   <div>
     <h1>vs {{ match_info.vs }}</h1>
-    <FormSchema :schema="schema" v-model="model" @submit.prevent="submit">
-      <button type="submit">Subscribe</button>
-    </FormSchema>
   </div>
 </template>
 
 <script>
 import json from "../assets/matches.json";
-import FormSchema from "@formschema/native";
-import schema from "../assets/matches.schema.json";
 
 export default {
-  data: () => ({
-    schema: schema,
-    model: {},
-  }),
   methods: {
     submit() {
       //submit here
@@ -27,7 +18,6 @@ export default {
       return json.matches.find((match) => match.id === parseInt(this.$route.params.id));
     },
   },
-  components: { FormSchema },
   name: "Single_Match",
 };
 </script>
