5eaa9cb3f32ee87fc37220bd77a1d36ffdc02e82
Nikolaos Katzakis <2638631+joobei@users.noreply.github.com>
2021-11-29 23:57:08 +0200

    replaced deprecated function atob(string)

--- src/shared/jwtHelper.js
+++ src/shared/jwtHelper.js
@@ -2,7 +2,7 @@ export function jwtDecrypt(token) {
   var base64Url = token.split(".")[1];
   var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
   var jsonPayload = decodeURIComponent(
-    atob(base64)
+    Buffer.from(base64,'base64')
       .split("")
       .map(function (c) {
         return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
