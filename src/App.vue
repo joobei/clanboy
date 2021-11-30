225c1538f56b86e1be5eb76f122bb0080579556f
Nikolaos Katzakis <2638631+joobei@users.noreply.github.com>
2021-11-30 23:50:03 +0200

    reduced navbar breakpoint

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
       <div class="container-fluid">
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
