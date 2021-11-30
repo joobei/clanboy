5a3d6f270ca7d0a4ae78b16d1bbb6c1d6e5cc0a8
Nikolaos Katzakis <2638631+joobei@users.noreply.github.com>
2021-12-01 00:31:15 +0200

    moved navbar to external component

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,15 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-sm navbar-light bg-light">
-      <div class="container-fluid">
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <router-link to="/" class="nav-link">Home</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="/login" class="nav-link">Login</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="/dashboard" class="nav-link">Dashboard</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="/matchlist" class="nav-link">Match List</router-link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <NavBar/>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import NavBar from "@/components/navbar.vue"
 export default {
   name: "App",
-  components: {},
+  components: { NavBar },
 };
 </script>
 
---
+++ src/components/navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="navbar navbar-expand-sm navbar-light bg-light">
+    <div class="container-fluid">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/login" class="nav-link">Login</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/dashboard" class="nav-link"
+              >Dashboard</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link to="/matchlist" class="nav-link"
+              >Match List</router-link
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  setup() {},
+};
+</script>
