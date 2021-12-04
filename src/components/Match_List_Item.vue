27bc23e1be5ecde7338824d7eb1ee4fbfd43dc40
Nikolaos Katzakis <2638631+joobei@users.noreply.github.com>
2021-12-05 01:30:26 +0200

    Parse Date and format it

--- src/components/Match_List_Item.vue
+++ src/components/Match_List_Item.vue
@@ -1,18 +1,14 @@
 <template>
   <router-link :to="{ name: 'single_match', params: { id: match_data.id } }">
     <dt class="col-sm-3">{{ match_data.vs }}</dt>
-    <dd class="col-sm-9">
-      {{ match_data.map }} - {{ match_data.date }}
-    </dd>
+    <dd class="col-sm-9">{{ match_data.map }} - {{ match_data.date }}</dd>
   </router-link>
 </template>
 
 <script>
-// import ListEditIcons from './ListEditIcons.vue';
 export default {
   name: "MatchListItem",
   props: ["match_data"],
-  // components: {ListEditIcons},
   data() {
     return {
       hover: false,
--- src/components/Single_Match.vue
+++ src/components/Single_Match.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="container">
     <h1>vs {{ match_info.vs }}</h1>
+    <dl class="row">
+      <dt class="col-sm-3">Map</dt>
+      <dd class="col-sm-9">{{ match_info.map }}</dd>
+      <dt class="col-sm-3">Date</dt>
+      <dd class="col-sm-9">{{ isoFormatDMY(parseISOString(match_info.date)) }}</dd>
+    </dl>
   </div>
 </template>
 
@@ -8,11 +14,29 @@
 import json from "../assets/matches.json";
 
 export default {
-  methods: { 
+  methods: {
+    isoFormatDMY(d) {
+      function pad(n) {
+        return (n < 10 ? "0" : "") + n;
+      }
+      return (
+        pad(d.getUTCDate()) +
+        "/" +
+        pad(d.getUTCMonth() + 1) +
+        "/" +
+        d.getUTCFullYear()
+      );
+    },
+    parseISOString(s) {
+      var b = s.split(/\D+/);
+      return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
+    },
   },
   computed: {
     match_info() {
-      return json.matches.find((match) => match.id === parseInt(this.$route.params.id));
+      return json.matches.find(
+        (match) => match.id === parseInt(this.$route.params.id)
+      );
     },
   },
   name: "Single_Match",
