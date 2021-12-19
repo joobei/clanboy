0038e9625c5b1815d281d4886e084d0e67f4fff5
Nikolaos Katzakis <2638631+joobei@users.noreply.github.com>
2021-12-19 21:52:59 +0200

    modal works!

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -26,25 +26,71 @@
           required
         />
       </div> -->
-
-      <div>
-        <button type="submit">Register</button>
-      </div>
+      <button
+        type="button"
+        class="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        Launch demo modal
+      </button>
     </form>
+  </div>
+  <!-- Modal -->
+  <div
+    class="modal fade"
+    id="exampleModal"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">...</div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            Close
+          </button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
+
+
 <script>
+// @ts-ignore
+import { Modal } from "bootstrap";
+
 export default {
   name: "RegiStration",
-  data() {
-    return {
-      username: "jubei",
-      email: "nkatzakis@gmail.com",
-      password: "amataro",
-      password_confirmation: "amataro",
-      is_admin: null,
-    };
+  data: () => ({
+    username: "jubei",
+    email: "nkatzakis@gmail.com",
+    password: "amataro",
+    password_confirmation: "amataro",
+    is_admin: null,
+    modal: null,
+  }),
+  mounted() {
+    this.modal = new Modal(document.getElementById("exampleModal"), {
+      keyboard: false,
+    });
   },
   methods: {
     register: function () {
