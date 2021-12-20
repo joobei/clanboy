c1f9847b4e471ccb43d2ad00a90f950934ae2bc4
Nikolaos Katzakis <2638631+joobei@users.noreply.github.com>
2021-12-20 18:57:57 +0200

    turned alert into a component

---
+++ src/components/AlertRow.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="row">
+    <div
+      class="alert alert-danger" style="width:100%"
+      align="center"
+      id="bottomAlert"
+    >
+      {{ message }}
+    </div>
+  </div>
+</template>
+
+  <script>
+export default {
+  name: "AlertRow",
+  props: ["message"],
+  // data: function () {
+  //   return {
+  //     alertMessage: "test",
+  //   };
+  // },
+};
+</script>
+  
