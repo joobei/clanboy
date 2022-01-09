a26add30e31970042736b4ab73d76196aa314d57
Nikolaos Katzakis <2638631+joobei@users.noreply.github.com>
2022-01-09 10:59:09 +0200

    removed fortawesome icons

--- src/main.js
+++ src/main.js
@@ -3,19 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store/index'
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faHouse, faRightToBracket, faGauge, faList, faBank, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
-library.add(faHouse);
-library.add(faRightToBracket);
-library.add(faRightFromBracket);
-library.add(faGauge);
-library.add(faList);
-library.add(faBank);
-
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
 createApp(App)
-.component("font-awesome-icon", FontAwesomeIcon)
 .use(router)
 .use(store)
 .mount('#app')
