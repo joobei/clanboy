08c437e9efe07ef9306ecafaeaaaa189c41d9b87
Nikolaos Katzakis <2638631+joobei@users.noreply.github.com>
2022-01-20 19:04:20 +0200

    added further fields to user

--- backend/user.js
+++ backend/user.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose"),
   passportLocalMongoose = require("passport-local-mongoose");
 var userSchema = new mongoose.Schema({
   discordId: String,
-  discordName: String
+  discordUsername: String,
+  discordAccessToken: String,
+  discordAvatar: String,
+  discordDiscriminator: String,
+  fetchedAt: Date
 });
 userSchema.plugin(passportLocalMongoose);
 module.exports = mongoose.model("User", userSchema);
