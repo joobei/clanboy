8311bc95d9bfe542b9bb419634413b013f063051
Nikolaos Katzakis <2638631+joobei@users.noreply.github.com>
2022-01-23 10:00:18 +0200

    removed legacy code

--- backend/match.js
+++ backend/match.js
@@ -11,17 +11,3 @@ const matchSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("Match", matchSchema);
-
-var express = require("express");
-var router = express.Router();
-var matchController = require("./matchController");
-var auth = require("../middleware/auth")();
-router.get("/matches", auth.authenticate(), matchController.get_matches);
-
-const Match = require("./match")
-
-exports.get_matches = async function (req, res) {
-  res.send(await Match.find());
-};
-
-module.exports = router;
