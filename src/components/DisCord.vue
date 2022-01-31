a8bc922cac9479c08330f2c01a3785cf088c00ab
Nikolaos Katzakis <2638631+joobei@users.noreply.github.com>
2022-02-01 01:06:20 +0200

    redirect to matches after login

--- src/components/DisCord.vue
+++ src/components/DisCord.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="container">
-    <p>Please wait while you are being logged in</p>
-    <h2>{{ discord_info }}</h2>
+    <h2>Please wait while you are being logged in</h2>
   </div>
 </template>
 <script>
+import { mapState } from "vuex";
 export default {
   name: "Discord_",
-  data() {
-    return { discord_info: null };
-  },
+  computed: mapState("auth", ["loginStatus"]),
   mounted() {
     console.log(this.$route.query.code);
     this.$store.dispatch("auth/discord_login", this.$route.query.code);
   },
+  watch: {
+    loginStatus: function() {
+      this.$router.push('/matchlist')
+    }
+  }
 };
 </script>
