2063e99a97decf8a9adb49969e25b6566a89b86e
Nikolaos Katzakis <2638631+joobei@users.noreply.github.com>
2022-02-04 00:39:02 +0100

    removed console logging

--- src/components/DisCord.vue
+++ src/components/DisCord.vue
@@ -9,7 +9,6 @@ export default {
   name: "Discord_",
   computed: mapState("auth", ["loginStatus"]),
   mounted() {
-    console.log(this.$route.query.code);
     this.$store.dispatch("auth/discord_login", this.$route.query.code);
   },
   watch: {
