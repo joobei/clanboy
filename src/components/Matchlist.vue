e4291e19982362d8dc4fe8d825d13230b01ead3d
Nikolaos Katzakis <2638631+joobei@users.noreply.github.com>
2022-02-24 01:45:05 +0100

    loading spinner for matches

--- src/components/Matchlist.vue
+++ src/components/Matchlist.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="container">
     <h3 style="margin-top: 20px">Upcoming Match</h3>
-    <dl class="row">
+    <div class="spinner-border .m-5" role="status" v-if="loading"></div>
+    <dl class="row" v-if="!loading">
       <MatchListItem
         style="margin-bottom: 1em"
         :class="{
           'rounded bg-light text-dark': index % 2 === 0,
           'rounded bg-secondary text-light': index % 2 !== 0,
         }"
-         v-for="(data, index) in getMatches"
+        v-for="(data, index) in matches"
         :key="index"
         v-bind:match_data="data"
       />
@@ -18,30 +19,33 @@
 
 <script>
 import MatchListItem from "./Match_List_Item.vue";
-import { mapActions, mapGetters } from "vuex";
+import { mapActions, mapState } from "vuex";
 
 export default {
   components: { MatchListItem },
   data() {
     return {
-      matchData: '',
-      loading: false
+      loading: true,
     };
   },
   name: "MatchList",
-  computed: {
-    ...mapGetters("matches", {
-      getMatches: "getMatchData",
-    }),
-  },
   methods: {
     ...mapActions("matches", {
       forceLoadMatches: "loadMatches",
     }),
   },
+  computed: mapState("matches", ["matches"]),
   created() {
-    this.matchData = this.getMatches;
     this.forceLoadMatches();
+  },
+  watch: {
+    matches: function () {
+      if (this.matches[0]) {
+        this.loading = false;
+        } else {
+        this.loading = true;
+      }
+    },
   },
 };
 </script>
